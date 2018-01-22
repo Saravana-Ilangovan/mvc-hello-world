2fef99868e59a72ba5a45021f53f123b2d39c5c1
Saravana Ilangovan <saravanasrv94@gmail.com>
2018-01-22 23:34:04 +0530

    check rebase

--- src/main/java/MvcHelloWorld.java
+++ src/main/java/MvcHelloWorld.java
@@ -3,6 +3,8 @@ package main.java;
 public class MvcHelloWorld {
 	public static void main(String args[]){
 		System.out.println("Initialized mvn repo");
+		// commit -1
+System.out.println("Commit 1 prepare to rebase");
 	}
 
 }
